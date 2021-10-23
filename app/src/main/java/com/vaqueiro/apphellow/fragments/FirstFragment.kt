bb639bcf2327562b0f64906cbe1ad8219c971882
jignaciovg <ignaciovaqdevelop@hotmail.com>
2021-10-23 09:12:24 -0500

    Implementacion ViewBinding con Fragments / FirstFragment

--- app/src/main/java/com/vaqueiro/apphellow/fragments/FirstFragment.kt
+++ app/src/main/java/com/vaqueiro/apphellow/fragments/FirstFragment.kt
@@ -8,10 +8,22 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.commit
 import com.vaqueiro.apphellow.R
+import com.vaqueiro.apphellow.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
 
-    private lateinit var btnFragment2: Button
+    //private lateinit var btnFragment2: Button
+    private var _binding:FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFirstBinding.inflate(inflater,container,false)
+        return binding.root
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +31,22 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        btnFragment2 = view.findViewById(R.id.btn_fragment2)
 
-        btnFragment2.setOnClickListener{
+        //IMPLEMENTACION DE BINDING
+        binding.btnFragment2.setOnClickListener{
             requireActivity().supportFragmentManager.commit {
                 replace(R.id.fragment_container_view,SecondFragment())
             }
         }
+
+        /*
+        FORMA TRADICIONAL
+        btnFragment2 = view.findViewById(R.id.btn_fragment2)
+        btnFragment2.setOnClickListener{
+            requireActivity().supportFragmentManager.commit {
+                replace(R.id.fragment_container_view,SecondFragment())
+            }
+        }*/
     }
 
 }
